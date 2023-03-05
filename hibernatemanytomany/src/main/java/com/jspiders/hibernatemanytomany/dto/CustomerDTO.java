6af1b309073d96e556d2eb6246b809b08b4b8d1e
Shubham Undre <shubhamundre7211@gmail.com>
2023-03-05 16:40:46 +0530

    Add files via upload

---
+++ hibernatemanytomany/src/main/java/com/jspiders/hibernatemanytomany/dto/CustomerDTO.java
@@ -0,0 +1,19 @@
+package com.jspiders.hibernatemanytomany.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class CustomerDTO {
+
+	@Id
+	private int id;
+	private String name;
+	private long contact;
+	private String city;
+	
+
+}
